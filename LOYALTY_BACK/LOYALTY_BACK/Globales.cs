3d1dde7f4a45a370c85f7669100caf34792729d3
Gonay-GRS <116356425+Gonay-GRS@users.noreply.github.com>
2024-07-25 11:12:29 +0200

    ficheros para conexion a servicios

---
+++ LOYALTY_BACK/LOYALTY_BACK/Globales.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOYALTY_BACK
+{
+    public class Globales
+    {
+
+        public static String VERSION = "1.0";
+        public static Boolean DEBUG_MODE = true;
+        public static string LOGS_DESTINATION_FOLDER = @"C:\loyalty\logs\loyalty.log";
+
+        public static String IP_SERVER = "localhost";
+        public static String PORT_SERVER = "3000";
+
+
+
+        /*static (string Codigo, int Puntos) GetFidelizacionInfo(string idCliente)
+        {
+            string connectionString = "your_connection_string_here"; // Actualiza con tu cadena de conexión
+            string codigoFidelizacion = string.Empty;
+            int puntosAcumulados = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT CodigoFidelizacion, Puntos FROM Clientes WHERE IdCliente = @IdCliente";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@IdCliente", idCliente);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            codigoFidelizacion = reader["CodigoFidelizacion"].ToString();
+                            puntosAcumulados = Convert.ToInt32(reader["Puntos"]);
+                        }
+                    }
+                }
+            }
+
+            return (codigoFidelizacion, puntosAcumulados);
+        }*/
+
+        /**
+         * Guarda el 'texto' en un fichero de log
+         **/
+        public static void CrearLog(String texto)
+        {
+            try
+            {
+
+                // Verificar si el archivo NO existe
+                if (!File.Exists(LOGS_DESTINATION_FOLDER))
+                {
+                    // Si no existe, crear el archivo y escribir contenido inicial
+                    using (StreamWriter writer = File.CreateText(LOGS_DESTINATION_FOLDER))
+                    {
+                        // Obtener la fecha actual
+                        DateTime fechaActual = DateTime.Now;
+
+                        // Agregar contenido al final del archivo
+                        writer.WriteLine($"{fechaActual}: " + texto);
+                    }
+                }
+
+                // Abrir el archivo en modo de escritura, agregando contenido al final
+                using (StreamWriter writer = File.AppendText(LOGS_DESTINATION_FOLDER))
+                {
+
+                    // Obtener la fecha actual
+                    DateTime fechaActual = DateTime.Now;
+
+                    // Agregar contenido al final del archivo
+                    writer.WriteLine($"{fechaActual}: " + texto);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error al abrir el log " + LOGS_DESTINATION_FOLDER + ": " + ex.Message);
+                MessageBox.Show("Error al abrir el log " + LOGS_DESTINATION_FOLDER + ": " + ex.Message);
+            }
+
+        }
+
+        /**
+         * Guarda el 'texto' en un fichero de log si la variable debug_mode esta a true
+         **/
+        public static void CrearLogDebug(String texto)
+        {
+            if (DEBUG_MODE)
+            {
+                try
+                {
+
+                    // Verificar si el archivo NO existe
+                    if (!File.Exists(LOGS_DESTINATION_FOLDER))
+                    {
+                        // Si no existe, crear el archivo y escribir contenido inicial
+                        using (StreamWriter writer = File.CreateText(LOGS_DESTINATION_FOLDER))
+                        {
+                            // Obtener la fecha actual
+                            DateTime fechaActual = DateTime.Now;
+
+                            // Agregar contenido al final del archivo
+                            writer.WriteLine($"{fechaActual}: " + texto);
+                        }
+                    }
+
+                    // Abrir el archivo en modo de escritura, agregando contenido al final
+                    using (StreamWriter writer = File.AppendText(LOGS_DESTINATION_FOLDER))
+                    {
+
+                        // Obtener la fecha actual
+                        DateTime fechaActual = DateTime.Now;
+
+                        // Agregar contenido al final del archivo
+                        writer.WriteLine($"{fechaActual}: " + texto);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al abrir el log " + LOGS_DESTINATION_FOLDER + ": " + ex.Message);
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ LOYALTY_BACK/LOYALTY_BACK/ServiciosConcentrador.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace LOYALTY_BACK
+{
+    internal class ServiciosConcentrador
+    {
+
+        public static async Task<object> GetExternalInfoTiketsAsync(string idCliente)
+        {
+            string status = "alive";
+            if (Int128.Parse(idCliente) % 3 == 0)
+            {
+                status = "dead";
+            }
+            else if (Int128.Parse(idCliente) % 3 == 1) status = "unknown";
+
+            //string externalServiceUrl = "https://rickandmortyapi.com/api/character/?status=" + status; // Actualiza con la URL del servicio externo
+
+            //http://192.168.253.89:8080/webfidelizacion/documentos?codigo=21468706&dias=590
+            string externalServiceUrl = "http://192.168.253.89:8080/webfidelizacion/documentos?codigo=" + idCliente + "&dias=590";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(externalServiceUrl);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var externalInfo = JsonSerializer.Deserialize<object>(responseBody); // Actualiza el tipo según el esquema del JSON devuelto por el servicio externo
+                return externalInfo;
+            }
+        }
+
+    }
+}
