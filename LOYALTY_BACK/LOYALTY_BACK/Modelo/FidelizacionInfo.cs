2906433ed5d9ffc9c2745bc52180b42caba61e1d
Gonay-GRS <116356425+Gonay-GRS@users.noreply.github.com>
2024-07-26 15:02:33 +0200

    ordenar en MVC

---
+++ LOYALTY_BACK/LOYALTY_BACK/Modelo/FidelizacionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOYALTY_BACK.Modelo
+{
+    internal class FidelizacionInfo
+    {
+        public string CodigoFidelizacion { get; set; }
+        public int PuntosAcumulados { get; set; }
+    }
+}
---
+++ LOYALTY_BACK/LOYALTY_BACK/Servicios/FidelizacionService.cs
@@ -0,0 +1,58 @@
+﻿using LOYALTY_BACK.Modelo;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOYALTY_BACK.Servicios
+{
+    internal class FidelizacionService
+    {
+
+        private readonly string _connectionString;
+
+        public FidelizacionService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public FidelizacionInfo GetFidelizacionInfo(string idCliente)
+        {
+            FidelizacionInfo fidelizacionInfo = null;
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT codigo, puntos FROM Clientes WHERE id = @IdCliente";
+                    using (var command = new NpgsqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@IdCliente", double.Parse(idCliente));
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                fidelizacionInfo = new FidelizacionInfo
+                                {
+                                    CodigoFidelizacion = reader["CodigoFidelizacion"].ToString(),
+                                    PuntosAcumulados = Convert.ToInt32(reader["Puntos"])
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al acceder a la base de datos: {ex.Message}");
+            }
+
+            return fidelizacionInfo;
+        }
+    }
+        
+}
